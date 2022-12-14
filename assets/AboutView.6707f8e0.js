1bd63c4f5a140f9e8ff591b362cad5a73d77cb75
Elias Mikkola <elias@singa.com>
2022-12-15 01:53:15 +0200

    deploy

--- assets/AboutView.6707f8e0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as _}from"./index.70ef5406.js";const s={},c={class:"about"},n=_("h1",null,null,-1),a=[n];function r(l,i){return t(),o("div",c,a)}const d=e(s,[["render",r]]);export{d as default};
