51ef187c3bfbdb2783dee643ad02867d465e6be1
Elias Mikkola <elias@singa.com>
2022-12-14 22:58:32 +0200

    nested build

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/helsinki-cycling-weather/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'index.html'),
+      nested: resolve(__dirname, 'dist/index.html'),
+    },
+  },
 })
